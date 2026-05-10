7136f448fe9b2212342d3504504eab23e008e379
Abbashatred <gitabbas26@gmail.com>
2026-05-10 16:21:18 +0400

    add account type filtering and generic exception handling

--- src/main/java/org/neobank/accountservice/repository/AccountRepository.java
+++ src/main/java/org/neobank/accountservice/repository/AccountRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.neobank.accountservice.enums.AccountType;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
     Optional<Account> findByIban(String iban);
     List<Account> findAllByUserId(UUID userId);
+    Optional<Account> findByUserIdAndAccountType(UUID userId, AccountType accountType);
 }
